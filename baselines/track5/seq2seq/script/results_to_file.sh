8590e94c2ad7e2e4ada9e127a834262d45963419
whale-z <102846153+whale-z@users.noreply.github.com>
2022-06-16 17:13:15 +0800

    Create results_to_file.sh

---
+++ baselines/track5/seq2seq/script/results_to_file.sh
@@ -0,0 +1,3 @@
+python results_to_file.py --test_path ./data_qe/test.phase1.fluency.json \
+--test_score_path ./data_feature/output_phase1_fluency.json \
+--output_path ./output/final_phase1_fluency.json
